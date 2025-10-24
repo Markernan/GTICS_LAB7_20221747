3a7df7ebbc92f46257247312731da35ab412ce31
Markernan <markernan@hotmail.com>
2025-10-23 19:39:17 -0500

    pusheo

---
+++ src/main/java/com/example/lab7_20221747/Lab720221747Application.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20221747;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab720221747Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab720221747Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab7_20221747/Lab720221747ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7_20221747;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab720221747ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
